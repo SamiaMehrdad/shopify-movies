e2f90ff34276497cc0de174ac87e0a8d9ddeacd0
msamia <samiamehrdad@gmail.com>
2021-05-07 20:07:08 -0700

    OMDB call test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React , {useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [searchResult, setSearchResult] = useState([]);
+  async function searchAPI( title )
+  {
+    const OMDB_URL = "http://www.omdbapi.com/?apikey=27c4a699&type=movie&s=";
+    let movieTitle = title.replace(' ', '+');
+    const res = await fetch( OMDB_URL+movieTitle )
+    if(res.ok) 
+        return res.json();
+    console.log("ERROR: ", res[0]);  
+  }
+
+  function formatedSearch( title )
+  {
+    searchAPI( title ).then( resObj => {
+    //console.log(resObj.Search);
+    let result = [];
+    if(resObj.Search )
+      resObj.Search.map( movie => {
+          let data = movie.Title+' ('+movie.Year+')';
+          result.push(data);
+      })
+    setSearchResult([...result]);
+    console.log(result);
+    return result;
+    });
+  }
+
+  
+  //console.log("S+",searchResult);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    The Shoppies
+
+    {formatedSearch ("rambo")}
+    {searchResult.length? 
+      searchResult.map( movie=> <div>{movie}</div> )
+      : <p> No matched movie </p>}
     </div>
   );
 }
