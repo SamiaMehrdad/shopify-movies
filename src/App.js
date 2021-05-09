73fc5c99fde9472ad6bc085ea4aae83a28b9c631
msamia <samiamehrdad@gmail.com>
2021-05-08 18:19:56 -0700

    add and remove nominees

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
   const [nominees, setNominees] = useState([]);
 
   //----------------------------------
+  // Async function to call API for search in titles and return json response
+  //----------------------------------
   async function searchAPI( title )
   {
     const OMDB_URL = "http://www.omdbapi.com/?apikey=27c4a699&type=movie&s="; // base URL for calling OMDB API
-    let movieTitle = title.replace(' ', '+'); // API call needs '+' instead of space. OMDB docs
+    const movieTitle = title.replace(' ', '+'); // API call needs '+' instead of space. OMDB docs
     const res = await fetch( OMDB_URL+movieTitle )
     if(res.ok) 
       return res.json();
@@ -20,9 +22,10 @@ function App() {
   
   //----------------------------------
   // Call searchAPI() and return array of results in format of MovieTitle(year)
+  //----------------------------------
   function formatedSearch( title )
   {
-    if( title.length < 3 )
+    if( title.length < 3 ) //searching for just 2 letters is useless 
     return;
     
     searchAPI( title ).then( resObj => {
@@ -45,6 +48,7 @@ function App() {
   
   //----------------------------------
   // Trim str to max and add '...' at the end if needed
+  //----------------------------------
   function stringLimiter( str, max )
   {
     let result = str;
@@ -57,18 +61,33 @@ function App() {
   
   //----------------------------------
   // Check if nominations<5 then add item to nominations 
+  //----------------------------------
   function addNom(e) 
   {
-    let name = e.currentTarget.getAttribute("item"); 
-    if( nominees.length < 5 && nominees.indexOf(name) < 0 )
-      setNominees( [...nominees, name] );
+    let item = e.currentTarget.getAttribute("item"); 
+    if( nominees.length < 5 ) // it is not ness in this version because <nominate> button will be hided after 5 nominee 
+    {
+      setNominees( [...nominees, item] );
+      const index = searchResult.indexOf(item);
+      searchResult.splice(index,1);
+      setSearchResult([...searchResult])  ;
+    }
+    // console.log(nominees, searchResult);
   }
   //----------------------------------
   // Remove item from nominees and add it back to searchResult
+  //----------------------------------
   const remNom = (e) =>{
-    
+    let item = e.currentTarget.getAttribute("item"); 
+    // remove item from nominees array
+    let index = nominees.indexOf( item );
+    nominees.splice(index, 1);
+    setNominees([...nominees]);
+    //add item back to searchResult list
+    setSearchResult( [...searchResult, item] );
+    console.log(nominees, searchResult);
   }
-  
+  //-----------------------------------------
   return (
     <div className="App">
     <h2>The Shoppies</h2>
@@ -79,7 +98,7 @@ function App() {
         {'\u{1F50D}'} {/* long unicode formatting: search icon */}
         <input type="text" 
                 item="search" 
-                placeholder="some letters"
+                placeholder="Enter three or more letters"
                 onChange={ 
                           e => {
                             e.preventDefault();
@@ -94,7 +113,6 @@ function App() {
       <div className="inner">
         {searchResult.length? searchResult.map(
                     (item) => 
-                      nominees.indexOf(item) < 0 ? 
                         <li className="nombar">
                           {item}
                           {nominees.length < 5 ?
@@ -105,7 +123,6 @@ function App() {
                             </span>
                           : null}
                         </li>
-                      : null
                       )
           : <p> No matched movie </p>
         }
@@ -115,14 +132,16 @@ function App() {
       <h4>Nominations</h4>
         <div className="inner">
         {nominees.length? nominees.map(
-                    item => <li className="nombar" >
-                            {item}
-                            <span className="hide"
-                                  name={item}
-                                  onClick={remNom}>
-                              Remove 
-                            </span>
-                           </li> )
+                    item => 
+                        <li className="nombar" >
+                          {item}
+                          <span className="hide"
+                                item={item}
+                                onClick={remNom}>
+                            Remove 
+                          </span>
+                          </li> 
+                      )
           : <p>Search and add up to 5 nominees.</p>
         } 
         { 
